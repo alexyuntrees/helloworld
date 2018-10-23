15426184c55feb68781c0c27691bce394543348f
alexyuntrees <btbrulesmars@gmail.com>
2018-10-22 20:01:07 -0700

    added cool stuff

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,8 @@
 print("Hello world")
 print("no u")
+print("reeeeeeeeeee")
+potatocounter = 2
+for (potatocounter<= 7; potatocounter++)
+    print("You have" + potatocounter + "potatoes")
+    if(potatocounter == 3)
+        print("i liek trey potat")
